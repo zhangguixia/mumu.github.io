b1fbe1fed90d2508e678a2e948c4fb625a87714c
zhangguixia <zhangguixia@bytedance.com>
2024-12-10 19:59:18 +0800

    feat:图片删除

--- src/components/Advert.js
+++ src/components/Advert.js
@@ -6,8 +6,8 @@ const Advert = () => {
 
     return(
         <div className="ad-div comm-box">
-             <div><img src="http://blogimages.jspang.com/flutter_ad2.jpg" width="100%"/></div>
-             <div><img src="http://blogimages.jspang.com/flutter_ad2.jpg" width="100%"/></div>
+             {/* <div><img src="http://blogimages.jspang.com/flutter_ad2.jpg" width="100%"/></div>
+             <div><img src="http://blogimages.jspang.com/flutter_ad2.jpg" width="100%"/></div> */}
         </div>
     )
 }
