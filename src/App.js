ac326e76c21c97b296dc36877ac46826eb7faa88
zhangguixia <zhangguixia@bytedance.com>
2024-12-13 11:28:28 +0800

    merge remote changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import routers from './router/routers'
+import routers from './router/routers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import './style/pages/comm.css'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="zhangguixia.github.io">   
           <Routes>
           {
-             routers.map((item,index)=> <Route key={index} path={item.path} element={<item.component/>}/>) 
+             routers.map((item,index)=> <Route key={index} path={item.path} element={<item.component/>}/>)   ////如果使用 BrowserRouter，需要添加 basename
           }
           </Routes>
     </BrowserRouter>
