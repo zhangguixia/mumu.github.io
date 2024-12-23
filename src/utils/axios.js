969166045afd76243f96693c0d05c34695c48752
zhangguixia <zhangguixia@bytedance.com>
2024-12-23 17:13:43 +0800

    update favicon and site title

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,8 +10,8 @@ import axios from'axios';
 
 
 //axios的配置文件,可以在这里去区分开发环境和生产环境等全局一些配置
-const devBaseUrl = 'http://api.k780.com/'
-const proBaseUrl = 'http://xxxxxxx.com/'
+const devBaseUrl = 'http://localhost:3000/'
+const proBaseUrl = 'https://ashublog.ink/'
 //process.env返回的是一个包含用户的环境信息,它可以去区分是开发环境还是生产环境
 export const BASE_URL = process.env.NODE_ENV=== 'development' ? devBaseUrl : proBaseUrl
 export const TIMEOUT = 5000
